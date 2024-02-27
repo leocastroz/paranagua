c5f102a4ef574e47c53855dafd9d43ed9a754f48
leocastroz <castroz.dev@gmail.com>
2024-02-27 14:26:32 -0300

    teste-final

--- src/pelucias.js
+++ src/pelucias.js
@@ -6,136 +6,136 @@ export default [
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   // {
-  //   "img": './src/assets/imgs/produts/20.jpg',
+  //   "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/20.jpg',
   //   "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   // },
   {
-    "img": './src/assets/imgs/produts/27.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/27.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },{
-    "img": './src/assets/imgs/produts/29.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/29.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },{
-    "img": './src/assets/imgs/produts/32.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/32.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },{
-    "img": './src/assets/imgs/produts/35.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/35.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/36.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/36.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/37.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/37.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/39.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/39.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/41.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/41.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/42.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/42.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/43.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/43.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/44.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/44.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/46.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/46.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/47.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/47.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/48.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/48.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/50.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/50.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/53.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/53.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/55.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/55.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/56.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/56.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/57.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/57.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/59.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/59.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/61.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/61.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/65.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/65.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/66.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/66.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/68.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/68.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/69.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/69.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/70.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/70.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/71.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/71.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/72.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/72.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/73.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/73.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/76.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/76.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/77.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/77.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/78.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/78.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/79.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/79.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
 ];
--- src/vasos.js
+++ src/vasos.js
@@ -1,104 +1,104 @@
 // data.js - adicionar somente os vasos
 export default [
   {
-    "img": './src/assets/imgs/produts/1.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/1.jpg',
     "imgStar": 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fpluspng.com%2Fimg-png%2Fstar-png-star-vector-png-transparent-image-2000.png&f=1&nofb=1&ipt=88e0a6359335d87dbde5076508513169f00e92355d647efa5a453738c9d3335e&ipo=images',
     "title": "Produto 1",
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/2.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/2.jpg',
     "imgStar": 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fpluspng.com%2Fimg-png%2Fstar-png-star-vector-png-transparent-image-2000.png&f=1&nofb=1&ipt=88e0a6359335d87dbde5076508513169f00e92355d647efa5a453738c9d3335e&ipo=images',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/3.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/3.jpg',
     "imgStar": 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fpluspng.com%2Fimg-png%2Fstar-png-star-vector-png-transparent-image-2000.png&f=1&nofb=1&ipt=88e0a6359335d87dbde5076508513169f00e92355d647efa5a453738c9d3335e&ipo=images',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },{
-    "img": './src/assets/imgs/produts/4.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/4.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },{
-    "img": './src/assets/imgs/produts/5.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/5.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },{
-    "img": './src/assets/imgs/produts/6.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/6.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/7.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/7.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/8.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/8.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/9.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/9.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/10.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/10.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/11.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/11.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/12.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/12.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/13.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/13.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/14.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/14.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/15.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/15.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/16.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/16.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/17.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/17.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/18.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/18.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/19.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/19.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/20.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/20.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/21.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/21.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/22.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/22.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/23.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/23.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/24.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/24.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
   {
-    "img": './src/assets/imgs/produts/25.jpg',
+    "img": 'https://raw.githubusercontent.com/leocastroz/paranagua/main/src/assets/imgs/produts/25.jpg',
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
 ];
