6ac764e0cddc1b0e66c6052d5cd222ded13b1a8d
leocastroz <castroz.dev@gmail.com>
2024-02-27 12:41:42 -0300

    teste-1

--- src/pelucias.js
+++ src/pelucias.js
@@ -1,7 +1,7 @@
 // data.js
 export default [
   {
-    "img": './assets/imgs/produts/14.jpg',
+    "img": '../src/assets/imgs/produts/1.jpg',
     "title": "Produto 1",
     "message": 'https://api.whatsapp.com/send?phone=+5541984559417&text=Olá Casa das Flores Paranagua! Gostaria de comprar este item.'
   },
